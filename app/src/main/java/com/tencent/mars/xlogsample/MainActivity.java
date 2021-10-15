ccde94daf1f6c569a1907cd732d09d3e50abdedc
wangweijun123 <262448350@qq.com>
2021-10-15 15:50:34 +0800

    Update MainActivity.java

--- app/src/main/java/com/tencent/mars/xlogsample/MainActivity.java
+++ app/src/main/java/com/tencent/mars/xlogsample/MainActivity.java
@@ -3,14 +3,12 @@ package com.tencent.mars.xlogsample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    public static final String TAG = "wangweijun";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +17,20 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
+        tv.setText(NdkSimple.getSingnaturePassword());
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+        NdkSimple ndkSimple = new NdkSimple();
+        Log.i(TAG, "c修改前 "+ndkSimple.name);
+        ndkSimple.changeName();
+        Log.i(TAG, "c修改后 "+ndkSimple.name);
+
+        Log.i(TAG, "c修改前age= "+NdkSimple.age);
+        NdkSimple.changeAge();
+        Log.i(TAG, "c修改后age= "+ndkSimple.age);
+        ndkSimple.callAddMethod();
+
+        ndkSimple.callAddMethod2(10, 30);
+        Point point = NdkSimple.createPoint();
+        Log.i(TAG, "point: x = "+point.getX()+" , y = "+point.getY());
+    }
 }
